22f4804427ef5b26f8b411a698e7189b46ff17f8
MiguelOrtega47 <miguelortega805@gmail.com>
2023-10-17 11:37:53 -0300

    card asimetricas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  API_KEY: '5e3e158db3d1bc1e3315260871615853',
+  API_KEY: '10431d8d784bd85e2fdd0cccfd0e8959',
   API_URL: 'https://api.openweathermap.org/data/2.5/'
 };
 
