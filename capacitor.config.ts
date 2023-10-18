9c294bbfcd75336b4b5028d135623edf2c7eb44d
MIguel Ortega <miguelortega805@gmail.com>
2023-10-18 15:52:20 -0300

    .

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.tebi.app',
+  appId: 'io.ionic.starter',
   appName: 'tebi-app',
   webDir: 'www',
   server: {
